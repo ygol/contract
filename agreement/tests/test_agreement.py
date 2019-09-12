52f4698e7ca94bd05b4dee3fe2f11a12b4d879a4
Yves Goldberg <admin@ygol.com>
2019-09-12 19:11:35 +0300

    [ADD] unittest for agreement

---
+++ agreement/tests/test_agreement.py
@@ -0,0 +1,13 @@
+# © 2019 Ygol InternetWork (Yves Goldberg <admin@ygol.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from odoo import fields
+from odoo.exceptions import ValidationError
+from odoo.tests import common
+
+
+class TestAgreement(common.SavepointCase):
+    """
+    Test name_get
+    """
